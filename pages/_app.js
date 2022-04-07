885734302bf435e0e4c755be16b74ddb7f669657
Firstname Lastname <your_email@youremail.com>
2022-04-07 17:05:35 +0100

    update

---
+++ pages/_app.js
@@ -0,0 +1,18 @@
+import "../styles/globals.css";
+import { ThemeProvider } from "next-themes";
+import { SessionProvider } from "next-auth/react";
+import { RecoilRoot } from "recoil";
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider session={session}>
+      <RecoilRoot>
+        <ThemeProvider attribute="class">
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </RecoilRoot>
+    </SessionProvider>
+  );
+}
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,91 @@
+import { AnimatePresence } from "framer-motion";
+import { getSession, useSession } from "next-auth/react";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useRecoilState } from "recoil";
+import { modalState, modalTypeState } from "../atoms/modalAtom";
+import Feed from "../components/Feed";
+import Header from "../components/Header";
+import Modal from "../components/Modal";
+import Sidebar from "../components/Sidebar";
+import Widgets from "../components/Widgets";
+import { connectToDatabase } from "../util/mongodb";
+
+export default function Home({ posts, articles }) {
+  const [modalOpen, setModalOpen] = useRecoilState(modalState);
+  const [modalType, setModalType] = useRecoilState(modalTypeState);
+  const router = useRouter();
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      // The user is not authenticated, handle it here.
+      router.push("/home");
+    },
+  });
+
+  return (
+    <div className="bg-[#F3F2EF] dark:bg-black dark:text-white h-screen overflow-y-scroll md:space-y-6">
+      <Head>
+        <title>Feed | LinkedIn</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+
+      <main className="flex justify-center gap-x-5 px-4 sm:px-12">
+        <div className="flex flex-col md:flex-row gap-5">
+          <Sidebar />
+          <Feed posts={posts} />
+        </div>
+        <Widgets articles={articles} />
+        <AnimatePresence>
+          {modalOpen && (
+            <Modal handleClose={() => setModalOpen(false)} type={modalType} />
+          )}
+        </AnimatePresence>
+      </main>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  // Check if the user is authenticated on the server...
+  const session = await getSession(context);
+  if (!session) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/home",
+      },
+    };
+  }
+
+  // Get posts on SSR
+  const { db } = await connectToDatabase();
+  const posts = await db
+    .collection("posts")
+    .find()
+    .sort({ timestamp: -1 })
+    .toArray();
+
+  // Get Google News API
+  const results = await fetch(
+    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      session,
+      articles: results.articles,
+      posts: posts.map((post) => ({
+        _id: post._id.toString(),
+        input: post.input,
+        photoUrl: post.photoUrl,
+        username: post.username,
+        email: post.email,
+        userImg: post.userImg,
+        createdAt: post.createdAt,
+      })),
+    },
+  };
+}
