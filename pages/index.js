441a45010dfefe5db2ee55aefaf0482eb03697ff
Firstname Lastname <your_email@youremail.com>
2022-04-07 22:36:09 +0100

    articals

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
-import { AnimatePresence } from "framer-motion";
-import { getSession, useSession } from "next-auth/react";
-import Head from "next/head";
-import { useRouter } from "next/router";
-import { useRecoilState } from "recoil";
-import { modalState, modalTypeState } from "../atoms/modalAtom";
-import Feed from "../components/Feed";
-import Header from "../components/Header";
-import Modal from "../components/Modal";
-import Sidebar from "../components/Sidebar";
-import Widgets from "../components/Widgets";
-import { connectToDatabase } from "../util/mongodb";
+import { AnimatePresence } from 'framer-motion';
+import { getSession, useSession } from 'next-auth/react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useRecoilState } from 'recoil';
+import { modalState, modalTypeState } from '../atoms/modalAtom';
+import Feed from '../components/Feed';
+import Header from '../components/Header';
+import Modal from '../components/Modal';
+import Sidebar from '../components/Sidebar';
+import Widgets from '../components/Widgets';
+import { connectToDatabase } from '../util/mongodb';
 
 export default function Home({ posts, articles }) {
   const [modalOpen, setModalOpen] = useRecoilState(modalState);
@@ -19,21 +19,21 @@ export default function Home({ posts, articles }) {
     required: true,
     onUnauthenticated() {
       // The user is not authenticated, handle it here.
-      router.push("/home");
+      router.push('/home');
     },
   });
 
   return (
-    <div className="bg-[#F3F2EF] dark:bg-black dark:text-white h-screen overflow-y-scroll md:space-y-6">
+    <div className='bg-[#F3F2EF] dark:bg-black dark:text-white h-screen overflow-y-scroll md:space-y-6'>
       <Head>
         <title>Feed | LinkedIn</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <Header />
 
-      <main className="flex justify-center gap-x-5 px-4 sm:px-12">
-        <div className="flex flex-col md:flex-row gap-5">
+      <main className='flex justify-center gap-x-5 px-4 sm:px-12'>
+        <div className='flex flex-col md:flex-row gap-5'>
           <Sidebar />
           <Feed posts={posts} />
         </div>
@@ -55,7 +55,7 @@ export async function getServerSideProps(context) {
     return {
       redirect: {
         permanent: false,
-        destination: "/home",
+        destination: '/home',
       },
     };
   }
@@ -63,14 +63,14 @@ export async function getServerSideProps(context) {
   // Get posts on SSR
   const { db } = await connectToDatabase();
   const posts = await db
-    .collection("posts")
+    .collection('posts')
     .find()
     .sort({ timestamp: -1 })
     .toArray();
 
   // Get Google News API
   const results = await fetch(
-    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}`
+    `https://newsapi.org/v2/top-headlines?country=us&apiKey=ae6fed86a98e4f46bb230f7242ad690a`
   ).then((res) => res.json());
 
   return {
